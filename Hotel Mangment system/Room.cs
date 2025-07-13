b0618e1091d1c9b224e93a3b7b4b9177f6183444
Seif Elboghdady <seifelboghdady@std.mans.edu.eg>
2025-07-13 22:58:23 +0300

    this is after add classes Room, Customer, reservation

---
+++ Hotel Mangment system/Room.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel_Mangment_system
+{
+    internal class Room
+    {
+        public int RoomNumber { get; set; }
+        public string RoomType { get; set; }
+        public double Price { get; set; }
+        public bool IsBooked { get; set; }
+
+        public void DisplayInfo()
+        {
+            //display Room
+        }
+        public void Vacate()
+        {
+            //exist form room 
+        }
+        public void Book()
+        {
+
+        }
+
+    }
+    internal class Customer
+    {
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Name { get; set; }
+        public string cardId { get; set; }
+
+        public void Displaycustomer()
+        {
+            //display Room
+        }
+        public void updateInfo()
+        {
+
+        }
+
+    }
+    internal class Reservation
+    {
+        public DateTime CheckInDate { get; set; }
+        public string Room { get; set; }
+        public string customer { get; set; }
+        public DateTime CheckOutDate { get; set; }
+
+        public void CalculateStayDuration()
+        {
+
+        }
+        public void GetReservationDetails()
+        {
+
+        }
+
+
+
+    }
+    public class ReportService
+    {
+        private List<Room> _rooms;
+        private List<Customer> _customers;
+        private List<Reservation> _reservations;
+    }
+
+}
--- Hotel Mangment system/Services.cs
+++ Hotel Mangment system/Services.cs
@@ -11,11 +11,33 @@ namespace Hotel_Mangment_system
         public void List_login()
         {
             Console.Clear();
-            Console.WriteLine("Welcome To Our Services\n1] Add Room\n2] Add Customer\n3] Make Reservation\n4] Generate Report");
-        }
-        public void AddRoom()
-        {
+            Console.WriteLine("Welcome To Our Services\n1] Room Management\n2] Customer Management\n3] Make Reservation\n4] Booking\n5] Payment\n6] Reporting");
+            int choose = int.Parse(Console.ReadLine());
 
         }
+        private void choose(int choose)
+        {
+            if (choose == 1)
+            {
+                //call Room Mangment
+            }
+            else if(choose == 2)
+            {
+                //call Customer mangment
+            }
+            else if (choose == 3)
+            {
+                //call booking
+            }
+            else if (choose == 4)
+            {
+                //call payment
+            }
+            else if (choose == 5)
+            {
+                //call Reporting
+            }
+        }
+        
     }
 }
